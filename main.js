4aa3fbf75d8725e5dac25db733a7d5d0d5cad08d
thewrecker123 <105166080+thewrecker123@users.noreply.github.com>
2023-01-29 10:43:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.image.fromURL('BirthdayImage.jpg, function(Img)}
+   
+    block_image_object: Image;)
+
+    block_image_object.scaleToWidth(700)
+    block_image_object.scaleToHeight(510)
+    block_image_object.set({
+        top:0,
+        left:0
+    });
+   }
+   
+
+
+
+
+
+    
+    
+
+function playSound(){
+    x.play();
+	
+}
